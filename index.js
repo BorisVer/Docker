a8f48bb87ba2d7f531cc8f113a0896a7bf3c436a
BorisVer <borisversonnen@gmail.com>
2026-03-22 21:55:55 +0200

    Attempt 4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Please work...</h1>')
+  res.send('<h1>containrrr instead...</h1>')
 })
 
 const PORT = 8080
